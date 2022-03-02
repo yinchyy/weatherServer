21effcaecf920f54e5989872402271df6f8869e3
Szymon Studniarz <szymek1019@gmail.com>
2022-03-02 23:14:16 +0100

    check if json file exists

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = 8081;
 
 app.get('/', (req, res) => {
+  try {
+    fs.readFileSync("test.json");
+    
+  }
+  catch (err) {
+    if (err.code === 'ENOENT') {
+      console.log("file not found");
+    }
+  }
   res.send('Hello World!')
 })
 
