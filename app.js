0c7a7c44a045d55e839a4a5ffcbe2d5cb7dbf6b7
Szymon Studniarz <szymek1019@gmail.com>
2022-03-04 17:03:01 +0100

    send json data on GET method

--- app.js
+++ app.js
@@ -2,18 +2,23 @@ const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = 8081;
+const weather = require("./weather.js");
+let data = new Array();
 
+data.push(new weather("Warsaw", 25.6, 35, 1000));
+console.log(JSON.stringify(data[0]));
 app.get('/', (req, res) => {
   try {
-    fs.readFileSync("test.json");
-    
+    fs.readFileSync("test.json");    
   }
   catch (err) {
     if (err.code === 'ENOENT') {
       console.log("file not found");
+
+      fs.appendFileSync("data.json","");
     }
   }
-  res.send('Hello World!')
+  res.send(`${JSON.stringify(data[0])}`);
 })
 
 app.listen(port, () => {
