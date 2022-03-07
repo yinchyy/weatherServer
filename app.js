e6719bf05cd5cbc36d77f3b7df55361ab4785da2
Szymon Studniarz <szymek1019@gmail.com>
2022-03-07 19:26:21 +0100

    get response as JSON, add data from POST request to array

--- app.js
+++ app.js
@@ -6,28 +6,29 @@ const weather = require("./weather.js");
 let data = new Array();
 
 data.push(new weather("Warsaw", 25.6, 35, 1000));
+data.push(new weather("Poznan", 25.6, 35, 1000));
 console.log(JSON.stringify(data[0]));
 app.get('/', (req, res) => {
   try {
-    fs.readFileSync("test.json");    
+    fs.readFileSync("data.json");
   }
   catch (err) {
     if (err.code === 'ENOENT') {
       console.log("file not found");
 
-      fs.appendFileSync("data.json","");
+      fs.appendFileSync("data.json", "");
     }
   }
-  res.send(`${JSON.stringify(data[0])}`);
-})
-
-app.post('/', (req, res) => {
-  req.on('data', (chunk) => {
-    console.log(chunk.toString('utf8'));
-  });
-
-  res.send("received post\n");
+  res.send(data);
 });
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  app.post('/', (req, res) => {
+    req.on('data', (chunk) => {
+      console.log(chunk.toString('utf8'));
+      data.push(JSON.parse(chunk.toString('utf8')));
+    });
+
+    return res.send("received post\n");
+  });
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
