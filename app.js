d70011d5a4646aaf13f1b1886d71a55faa48bab8
Szymon Studniarz <szymek1019@gmail.com>
2022-03-09 17:00:01 +0100

    use body-parser, verify integrity of json

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const fs = require('fs');
 const express = require('express');
+const bodyParser = require("body-parser");
 const app = express();
 const port = 8081;
 const weather = require("./weather.js");
 const { json } = require('express/lib/response');
 const JSONStorageFile = "./data.json";
 const data = JSON.parse(getJSONData(JSONStorageFile).toString('utf-8'));
+const requiredParameters = ["location", "temperature", "humidity", "pressure"];
 
+app.use(bodyParser.json());
 
 appendOrModifyObjectInArray(data,new weather("Warsaw", 25.6, 35, 1000));
 appendOrModifyObjectInArray(data,new weather("Poznan", 25.6, 35, 1000));
@@ -50,23 +53,34 @@ function deleteObjectIfExists(dataArr, obj) {
     console.log("requested object doesn't exist");
   }
 }
+function isObjectIntegral(requiredParameters,obj) {
+  for (let key of requiredParameters) {
+    if (Object.keys(obj).findIndex((k) => key === k) === -1) {
+      return false;
+    }
+  }
+  return true;
+}
 app.get('/', (req, res) => {
   res.send(data);
 });
 app.post('/', (req, res) => {
-  req.on('data', (chunk) => {
-    console.log(chunk.toString('utf8'));
-    appendOrModifyObjectInArray(data,JSON.parse(chunk.toString('utf8')));
-    writeJSONData(JSONStorageFile);
-  });
-  return res.send("received post\n");
+  console.log(req.body);
+    if (!isObjectIntegral(requiredParameters,req.body)) {
+      return res.status(500).send({
+        error: `data should be sent in JSON format and contain fields: ${requiredParameters}. Check it and try again.`
+     });
+    }
+    else { 
+      appendOrModifyObjectInArray(data,req.body);
+      writeJSONData(JSONStorageFile);
+    }
+  return res.send(`received post request of ${req.body.location}\n`);
 });
 app.delete('/', (req, res) => {
-  req.on('data', (chunk) => {
-    console.log(chunk.toString('utf8'));
-    deleteObjectIfExists(data, JSON.parse(chunk.toString('utf8')));
-  });
-  return res.send("received delete\n");
+  console.log(req.body);
+  deleteObjectIfExists(data,req.body);
+  return res.send(`received delete request of ${req.body.location}\n`);
 });
 
 app.listen(port, () => {
